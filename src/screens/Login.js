135906728a52f4a7b04b4fac6aafce6dffc4ceec
pamccombs <34119196+pamccombs@users.noreply.github.com>
2020-06-13 16:57:20 -0400

    add login screen

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,29 +1,43 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 
+import Logo from '../components/Logo'
+import Form from '../components/Form'
+
 export default function Login() {
-    return (
-      <View style={styles.container}>
-        <StatusBar
-          backgroundColor='grey'
-          barStyle='light-content'
-        />
-        <Text style={styles.text}>Login</Text>
+  return (
+    <View style={styles.container}>
+      <Logo />
+      <Form type="Login"/>
+      <View style={styles.signupTextCont}>
+        <Text style={styles.signupText}>Don't have an account yet?</Text>
+        <Text style={styles.signupButton}> Signup!</Text>
       </View>
-    );
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#455a64',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  signupTextCont: {
+    flexGrow: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    paddingVertical: 16,
+    flexDirection: 'row'
+  },
+  signupText: {
+    color: 'rgba(255, 255, 255, 0.7)',
+    fontSize: 16
+  },
+  signupButton: {
+    color: '#ffffff',
+    fontSize: 16,
+    fontWeight: '500'
   }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#455a64',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  
-    text: {
-      color: '#ffffff',
-      fontSize: 18
-    }
-  });
-  
+});
